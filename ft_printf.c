67c5a46bbdf9b94d9458a83eb7498fc09ea42965
NaomisNuddles <nmla0012@gmail.com>
2024-11-25 16:29:26 +0000

    trading alias

---
+++ ft_printf.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nleandro <nleandro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 12:06:31 by nleandro          #+#    #+#             */
+/*   Updated: 2024/11/25 16:29:12 by nleandro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *end, ...)
+{
+
+	alias t="./t"
+alias c="clear"
+alias cf="gcc -o t -Wall -Wextra -Werror"
+alias cff="gcc -o t"
+alias ft="gcc -o t -Wall -Wextra -Werror *.c"
+alias ftt="gcc -o t *.c"
+alias nr="norminette"
+
+}
+
+int	main()
+{
+
+}
